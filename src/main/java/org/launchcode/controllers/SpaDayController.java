a3cb9544c8f5fd87b591f3404548a472ad6dbda5
Sara Bohannon <your.email@example.com>
2024-12-05 20:08:37 -0600

    studio

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -63,6 +63,13 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi",manipedi);
+        model.addAttribute("facials",facials);
+        model.addAttribute("appropriateFacials",appropriateFacials);
+        model.addAttribute("color", color);
+
         return "menu";
     }
 }
